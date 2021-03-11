3acf8144948449c2556067c5ffadb5e4ab987f35
David Beale <david.beale@bealearts.co.uk>
2021-03-11 16:06:47 +0000

    Integration test

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
---
+++ test/integration.test.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+const { node } = require('execa');
+const { resolve } = require('path');
+
+let serverlessProcess;
+
+const serverlessPath = resolve(
+  __dirname,
+  '../node_modules/serverless/bin/serverless'
+);
+
+async function setup() {
+  serverlessProcess = node(serverlessPath, ['offline', 'start', '--config', './example/serverless.yaml',
+    '--noPrependStageInUrl', '--httpPort', '8765', '--lambdaPort', '8432'], {
+    cwd: './'
+  });
+
+  await new Promise((res) => {
+    serverlessProcess.stdout.on('data', (data) => {
+      if (String(data).includes('[HTTP] server ready')) {
+        res();
+      }
+    });
+  });
+}
+
+async function teardown() {
+  await serverlessProcess.cancel();
+}
+
+test('Starts and seeds a MongoDB instance', async () => {
+  await setup();
+
+  const response = await fetch('http://localhost:8765');
+  expect(response.ok).toEqual(true);
+
+  const result = await response.json();
+  expect(result.length).toEqual(3);
+
+  await teardown();
+}, 10000);
