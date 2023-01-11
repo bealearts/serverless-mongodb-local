9fef88dea1ab2547f24d9b83633d9029cc8f7b54
Amit Gupta <amitgupta4b@gmail.com>
2023-01-11 12:46:08 +0530

    allowing _id to be taken up in seed data

--- src/seed.js
+++ src/seed.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 let client;
 
@@ -11,7 +11,11 @@ module.exports = async function seed(dataPath, uri, dbName, log) {
     await Promise.all(jsonfiles.map(async (file) => {
       const collection = path.basename(file, '.json');
       try {
-        const data = await fs.readJson(path.join(dataPath, file));
+        let data = await fs.readJson(path.join(dataPath, file));
+        data = data.map(p => {
+          if(p["_id"] && p["_id"]["$oid"]) p["_id"] = ObjectId(p["_id"]["$oid"]);
+          return p;
+        });
         const db = await getDb(uri, dbName);
         const result = await db.collection(collection).insertMany(data);
         log(`Seeded collection "${collection}" with ${result.insertedCount} documents`);
